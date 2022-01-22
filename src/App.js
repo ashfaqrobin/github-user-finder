9c5f4e30b49db3f291f9136957ac8e00ba99fc82
Ashfaq Robin <ashfaq.robin36@gmail.com>
2022-01-22 12:18:55 +0600

    Install and add daisyui tailwind plugin

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 const App = () => {
     return (
-        <div className="flex justify-center items-center min-h-[50vh]">
-            <h1 className="text-green-500 text-[50px]">Github User Finder</h1>
+        <div className="bg-purple-500">
+            <h1 className="text-xl">Github User Finder</h1>
+            <button className="btn">Click</button>
         </div>
     );
 };
