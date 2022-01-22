c9b6ced94729edc7e54f3c89f11ac578835f75f9
Ashfaq Robin <ashfaq.robin36@gmail.com>
2022-01-22 13:44:25 +0600

    -> Update style in Footer component. -> Create Home, About, and 404 page components in 'pages' folder. -> Add '/home', '/about', and '/not-found' routes in App component.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Footer from './components/layouts/Footer';
 import Navbar from './components/layouts/Navbar';
+import NotFound from './pages/404';
+import About from './pages/About';
+import Home from './pages/Home';
 
 const App = () => {
     return (
         <Router>
             <div className="flex flex-col justify-between h-screen">
                 <Navbar />
-                <main className="container px-3 pb-12 mx-auto">Content</main>
+                <main className="container px-3 pb-12 mx-auto">
+                    <Routes>
+                        {/* Home page route */}
+                        <Route exact path="/" element={<Home />} />
+
+                        {/* About page route */}
+                        <Route path="/about" element={<About />} />
+
+                        {/* 404 page route */}
+                        <Route path="/not-found" element={<NotFound />} />
+                        <Route path="/*" element={<NotFound />} />
+                    </Routes>
+                </main>
                 <Footer />
             </div>
         </Router>
--- src/components/layouts/Footer.jsx
+++ src/components/layouts/Footer.jsx
@@ -2,7 +2,7 @@ const Footer = () => {
     const footerYear = new Date().getFullYear();
 
     return (
-        <footer className="footer footer-center p-10 text-primary-content bg-gray-700">
+        <footer className="footer footer-center border-t-[1px] border-solid border-gray-800 p-10 text-primary-content bg-gray-700">
             Copyright &copy; {footerYear} | All Rights Reserved
         </footer>
     );
